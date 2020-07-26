f20c11a35371dab2bcda7499f1fc1a48e37fc94f
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-07-26 16:49:53 +0300

    add format validator

--- cnp.py
+++ cnp.py
@@ -1,3 +1,12 @@
+import string
+
 class CNP:
-    pass
+    @staticmethod
+    def has_cnp_format(cnp):
+        if len(cnp) != 13:
+            return False
+        for d in cnp:
+            if d not in string.digits:
+                return False
+        return True
 
--- test_cnp.py
+++ test_cnp.py
@@ -1,4 +1,9 @@
+from cnp import CNP
+
+
 class TestCNP:
-    def test_ok(self):
-        pass
+    def test_cnp_format(self):
+        assert not CNP.has_cnp_format('abc')
+        assert not CNP.has_cnp_format('1234')
+        assert CNP.has_cnp_format('1234567890123')
 
